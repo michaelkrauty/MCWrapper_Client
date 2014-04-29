71fafd625aff88c4d7e9ae7aa36a15af4add461b
michaelkrauty <michaelkrauty@gmail.com>
2014-04-29 14:57:50 -0700

    update

--- src/me/michaelkrauty/MCWrapperClient/Main.java
+++ src/me/michaelkrauty/MCWrapperClient/Main.java
@@ -3,9 +3,11 @@ package me.michaelkrauty.MCWrapperClient;
 import java.io.IOException;
 
 public class Main {
+
+	public static ConnectionHandler connectionHandler = new ConnectionHandler();
+
 	public static void main(String[] args) throws IOException {
 
-		ConnectionHandler connectionHandler = new ConnectionHandler();
 		connectionHandler.start();
 
 	}
